72a769924369257495854a147d69f7c23bfba144
song <songhsm@daum.net>
2020-06-04 11:09:07 +0900

    first

--- app/components/UITest2.vue
+++ app/components/UITest2.vue
@@ -36,7 +36,6 @@ export default {
 			this.incrementinit();
 
 			this.polling = setInterval(() => {
-				// test 
 				this.$store.dispatch("updateValue");
 				//this.$store.commit("increment", 2);
 			}, 1000);
